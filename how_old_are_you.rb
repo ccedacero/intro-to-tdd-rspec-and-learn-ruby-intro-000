53d690714f1f9e039fa29334c5256d7e49876241
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-03-13 22:26:28 +0000

    Automatically backed up by Learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -3,7 +3,7 @@ require_relative './current_age_for_birth_year.rb'
 puts "what year were you born?"
 birth_year = gets.to_i
 
-user_age = current_age_for_birth_year(birth_year)
+users_age = current_age_for_birth_year(birth_year)
 
 puts "You are: " + users_get.to_s + " years old."
 
